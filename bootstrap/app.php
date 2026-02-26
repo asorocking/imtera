273da4a3cfd489b869f83ef684fb482dc6a44178
Alexander Sorokin <a.sorocking@gmail.com>
2026-02-26 19:19:39 +0300

    Allowed work through http

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,9 @@ return Application::configure(basePath: dirname(__DIR__))
 
         //
     })
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
